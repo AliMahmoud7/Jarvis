e7b7dfe880084e6a08e8f0157446db725c1dd186
Ali Mahmoud <ali.mahmoud@engineer.com>
2017-12-17 21:25:46 +0200

    Add Initial test & project structure

---
+++ main.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+from app.features.respond.tts import tts
+
+
+# Suppress ALSA lib error messages
+# ----------------------------------------------------
+from ctypes import *
+
+# Define our error handler type
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
+
+def py_error_handler(filename, line, function, err, fmt):
+    pass
+
+
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+asound = cdll.LoadLibrary('libasound.so')
+# Set error handler
+asound.snd_lib_error_set_handler(c_error_handler)
+
+# Reset to default error handler
+# asound.snd_lib_error_set_handler(None)
+# ----------------------------------------------------
+
+
+name = 'Ali'
+tts('Welcome ' + name + ', systems are now ready to run. How can I help you?')
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone(sample_rate=48000) as source:
+    r.adjust_for_ambient_noise(source)  # listen for 1 second to calibrate the energy threshold for ambient noise levels
+    print("Say something!")
+    audio = r.listen(source, timeout=1, phrase_time_limit=3)
+
+# write audio to a WAV file
+with open("microphone-results.wav", "wb") as f:
+    f.write(audio.get_wav_data())
+
+speech_text = ''
+# recognize speech using Sphinx
+try:
+    speech_text = r.recognize_sphinx(audio)
+    print("Sphinx thinks you said " + speech_text)
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
+
+# recognize speech using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    speech_text = r.recognize_google(audio)
+    print("Google Speech Recognition thinks you said " + speech_text)
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+tts(speech_text)
+tts('Bye My friend, ' + name)
+
+'''
+# recognize speech using Wit.ai
+WIT_AI_KEY = "INSERT WIT.AI API KEY HERE"  # Wit.ai keys are 32-character uppercase alphanumeric strings
+try:
+    print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
+except sr.UnknownValueError:
+    print("Wit.ai could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Wit.ai service; {0}".format(e))
+
+
+# recognize speech using IBM Speech to Text
+IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
+try:
+    print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
+except sr.UnknownValueError:
+    print("IBM Speech to Text could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from IBM Speech to Text service; {0}".format(e))
+'''
