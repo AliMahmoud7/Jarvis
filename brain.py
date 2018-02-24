05174034ef6d8ac22e81bc208736b5d3050ad62c
Ali Mahmoud <ali.mahmoud@engineer.com>
2018-02-24 17:37:27 +0200

    Add brain

--- brain.py
+++ brain.py
@@ -1,1 +1,33 @@
-# You can edit this file seperate from master branch :: Ali
+from features import *
+
+
+def brain(name, speech_text):
+    """
+
+    :param name: user name founded in the profile
+    :param speech_text:
+    :return: TTS
+    """
+    def check_message(check):
+        words_of_message = speech_text.split()
+        if set(check).issubset(set(words_of_message)):
+            return True
+        else:
+            return False
+
+    if check_message(['who', 'are', 'you']):
+        who_are_you()
+    elif check_message(['how', 'i', 'look']) or check_message(['how', 'am', 'i']):
+        how_am_i()
+    elif check_message(['tell', 'joke']):
+        tell_joke()
+    elif check_message(['who', 'am', 'i']):
+        who_am_i(name)
+    elif check_message(['where', 'born']):
+        where_born()
+    elif check_message(['how', 'are', 'you']):
+        how_are_you()
+    elif check_message(['time']):
+        what_is_time()
+    else:
+        undefined()
