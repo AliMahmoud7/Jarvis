3b9fd29a50bfddb8f5035cfcc264abfd6c7979be
Your Name <you@example.com>
2018-07-11 17:56:47 +0200

    Solve mobile bug

--- app/static/js/chat.js
+++ app/static/js/chat.js
@@ -1,47 +1,49 @@
-let chatBox = $('#chatbox');
-let userInput = $('#usermsg');
-let userMsg = $('<div></div>').addClass('chat-msg right-msg').attr('dir', 'auto');
-let serverMsg = $('<div></div>').addClass('chat-msg left-msg').attr('dir', 'auto');
-let chatWaiting = $('<div><span></span><span></span><span></span></div>').addClass('chat-waiting');
-const focusedElem = $('<a href="#" style="display: block; clear: both; padding-top: 15px; cursor: default;"></a>').attr('id', 'focused_elem');
+$(function () {
+    var chatBox = $('#chatbox');
+    var userInput = $('#usermsg');
+    var userMsg = $('<div></div>').addClass('chat-msg right-msg').attr('dir', 'auto');
+    var serverMsg = $('<div></div>').addClass('chat-msg left-msg').attr('dir', 'auto');
+    var chatWaiting = $('<div><span></span><span></span><span></span></div>').addClass('chat-waiting');
+    var focusedElem = $('<a href="#" style="display: block; clear: both; padding-top: 15px; cursor: default;"></a>').attr('id', 'focused_elem');
 
-$('#text_form').submit(function (e) {
-    e.preventDefault();  // stop submit the form
+    $('#text_form').submit(function (e) {
+        e.preventDefault();  // stop submit the form
 
-    const InputValue = userInput.val();
-    const newUserMsg = userMsg.clone();
-    const newServerMsg = serverMsg.clone();
+        var InputValue = userInput.val();
+        var newUserMsg = userMsg.clone();
+        var newServerMsg = serverMsg.clone();
 
-    chatBox.append(newUserMsg.text(InputValue));
-    // newUserMsg.scrollTop(newUserMsg[0].scrollHeight);
-    userInput.val('');  // .removeAttr('value');
+        chatBox.append(newUserMsg.text(InputValue));
+        // newUserMsg.scrollTop(newUserMsg[0].scrollHeight);
+        userInput.val('');  // .removeAttr('value');
 
-    chatBox.append(newServerMsg.append(chatWaiting));
-    chatBox.append(focusedElem);
-    $('#focused_elem').focus();
-
-    $.ajax({
-        type: 'POST',
-        url: '/text',
-        contentType: 'text/plain; charset: UTF-8',
-        data: InputValue,
-        dataType: 'text'
-    }).done(function (res) {
-        // chatWaiting.remove();
-        newServerMsg.empty();
-        newServerMsg.text(res);
-
+        chatBox.append(newServerMsg.append(chatWaiting));
+        chatBox.append(focusedElem);
         $('#focused_elem').focus();
-        console.log(res);
+
+        $.ajax({
+            type: 'POST',
+            url: '/text',
+            contentType: 'text/plain; charset: UTF-8',
+            data: InputValue,
+            dataType: 'text'
+        }).done(function (res) {
+            // chatWaiting.remove();
+            newServerMsg.empty();
+            newServerMsg.text(res);
+
+            $('#focused_elem').focus();
+            console.log(res);
+
+            userInput.focus();
+
+        }).fail(function (err) {
+            newServerMsg.remove();
+            // newServerMsg.empty().text('An unexpected error has occurred on the server!');
+            console.log(err);
+        });
 
         userInput.focus();
 
-    }).fail(function (err) {
-        newServerMsg.remove();
-        // newServerMsg.empty().text('An unexpected error has occurred on the server!');
-        console.log(err);
     });
-
-    userInput.focus();
-
 });
