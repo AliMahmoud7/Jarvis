2605156d24900b5d757cc9ec0edf6fa4920504e5
Your Name <you@example.com>
2018-07-12 00:45:49 +0200

    Update color sensor

--- color_sensor.py
+++ color_sensor.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-
+from app.views.features.respond.tts import tts
 
 
 s2 = 22
@@ -10,75 +10,73 @@ NUM_CYCLES = 10
 
 
 def setup():
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(signal,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(s2,GPIO.OUT)
-  GPIO.setup(s3,GPIO.OUT)
-  print("\n")
-  
-
-
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(signal, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(s2, GPIO.OUT)
+    GPIO.setup(s3, GPIO.OUT)
+    print("\n")
 
 
 def loop():
-  temp = 1
-  while(1):  
+    temp = 1
+    while (1):
 
-    GPIO.output(s2,GPIO.LOW)
-    GPIO.output(s3,GPIO.LOW)
-    time.sleep(0.3)
-    start = time.time()
-    for impulse_count in range(NUM_CYCLES):
-      GPIO.wait_for_edge(signal, GPIO.FALLING)
-    duration = time.time() - start 
-    red  = NUM_CYCLES / duration   
-    print("red value - ",red)
+        GPIO.output(s2, GPIO.LOW)
+        GPIO.output(s3, GPIO.LOW)
+        time.sleep(0.3)
+        start = time.time()
+        for impulse_count in range(NUM_CYCLES):
+            GPIO.wait_for_edge(signal, GPIO.FALLING)
+        duration = time.time() - start
+        red = NUM_CYCLES / duration
+        print("red value - ", red)
 
-   
-    GPIO.output(s2,GPIO.LOW)
-    GPIO.output(s3,GPIO.HIGH)
-    time.sleep(0.3)
-    start = time.time()
-    for impulse_count in range(NUM_CYCLES):
-      GPIO.wait_for_edge(signal, GPIO.FALLING)
-    duration = time.time() - start
-    blue = NUM_CYCLES / duration
-    print("blue value - ",blue)
+        GPIO.output(s2, GPIO.LOW)
+        GPIO.output(s3, GPIO.HIGH)
+        time.sleep(0.3)
+        start = time.time()
+        for impulse_count in range(NUM_CYCLES):
+            GPIO.wait_for_edge(signal, GPIO.FALLING)
+        duration = time.time() - start
+        blue = NUM_CYCLES / duration
+        print("blue value - ", blue)
 
+        GPIO.output(s2, GPIO.HIGH)
+        GPIO.output(s3, GPIO.HIGH)
+        time.sleep(0.3)
+        start = time.time()
+        for impulse_count in range(NUM_CYCLES):
+            GPIO.wait_for_edge(signal, GPIO.FALLING)
+        duration = time.time() - start
+        green = NUM_CYCLES / duration
+        print("green value - ", green)
 
-    GPIO.output(s2,GPIO.HIGH)
-    GPIO.output(s3,GPIO.HIGH)
-    time.sleep(0.3)
-    start = time.time()
-    for impulse_count in range(NUM_CYCLES):
-      GPIO.wait_for_edge(signal, GPIO.FALLING)
-    duration = time.time() - start
-    green = NUM_CYCLES / duration
-    print("green value - ",green)
+        # time.sleep(3)
+        print('temp= %s \n' % temp)
 
-    #time.sleep(3)  
-    print('temp= %s \n' % temp)
-
-    if red>10000:
-      print("red")
-      temp=1
-    elif green>9000:
-      print("green")
-      temp=1
-    elif blue>13000:
-      print("blue")
-      temp=1
-    elif red>6000 and green>6000 and blue>6000 and temp==1:
-      print("place the object.....")
-      temp=0
-    print('\n')
+        if red > 10000:
+            print("red")
+            tts('STOP!')
+            temp = 1
+        elif green > 9000:
+            print("green")
+            tts('MOVE!')
+            temp = 1
+        elif blue > 13000:
+            print("blue")
+            temp = 1
+        elif red > 6000 and green > 6000 and blue > 6000 and temp == 1:
+            print("place the object.....")
+            temp = 0
+        print('\n')
 
 
 def endprogram():
     GPIO.cleanup()
 
-if __name__=='__main__':
-    
+
+if __name__ == '__main__':
+
     setup()
 
     try:
@@ -86,4 +84,3 @@ if __name__=='__main__':
 
     except KeyboardInterrupt:
         endprogram()
-
